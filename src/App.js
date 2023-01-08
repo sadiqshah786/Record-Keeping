19ff2d696ecf6958dad7f5971c91b1525ea05edc
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2023-01-08 20:24:16 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Feild from './Component/Feild';
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign:"center"}}>Project: Keeping Record</h1>
-       <Feild/>
+      <h1 style={{ textAlign: "center" }}>Project: Keeping Record</h1>
+      <Feild />
     </div>
   );
 }
--- src/Component/Feild.jsx
+++ src/Component/Feild.jsx
@@ -7,57 +7,48 @@ const Feild = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [data, setData] = useState([]);
-
     const getData = () => {
-        name!=="" && email!=="" && setData([...data, {name,email}])
-        
+        name !== "" && email !== "" && setData([...data, { name, email }])
         setName("")
         setEmail("")
-        
-        
+
+
     }
     return (
-       <div className="container">
-         <div className='form'>
+        <div className="container">
+            <div className='form'>
+                <div className="main">
+                    <Stack direction="row" spacing={2}>
+                        <TextField className='input' id="outlined-basic" label="name" variant="outlined" value={name} onChange={(e) => setName(e.target.value)} />
+                        <TextField className='input' id="outlined-basic" label="Email" value={email} variant="outlined" onChange={(e) => setEmail(e.target.value)} />
+                        <Button variant="contained" onClick={getData}><AddIcon /></Button>
+                    </Stack>
+                </div>
+                <div className="main_table">
 
-<div className="main">
-    <Stack direction="row" spacing={2}>
-        <TextField className='input' id="outlined-basic" label="name" variant="outlined"  value={name} onChange={(e) => setName(e.target.value)} />
-        <TextField className='input'  id="outlined-basic" label="Email"  value={email} variant="outlined" onChange={(e) => setEmail(e.target.value)} />
-        <Button variant="contained" onClick={getData}><AddIcon /></Button>
-    </Stack>
-</div>
-<div className="main_table">
-
-<Table striped>
-<thead>
-<tr>
-<th>Name</th>
-<th>Email</th>
-</tr>
-</thead>
-<tbody>
-{
-
-        data.map((ele,i)=>{
-            return(
-                
-                <tr key={i}>
-                <td>{ele.name}</td>
-                <td>{ele.email}</td>
-              </tr>
-
-            )
-        })
-    }
-
-
-</tbody>
-</Table>
-</div>
-</div>
-       </div>
-
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map((ele, i) => {
+                                    return (
+                                        <tr key={i}>
+                                            <td>{ele.name}</td>
+                                            <td>{ele.email}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
+        </div>
     )
 }
 
