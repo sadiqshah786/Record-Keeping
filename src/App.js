b8ce15a9cbf4f1cca62b48d5da6c795310baa03a
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2023-01-08 15:37:14 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Feild from './Component/Feild';
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{textAlign:"center"}}>Project: Keeping Record</h1>
+       <Feild/>
+    </div>
+  );
+}
+
+export default App;
